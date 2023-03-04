b2a5a65a4c1622a878ab33e055a5584766ec95f1
NATASHA-ct <chirombenatashatatenda@gmail.com>
2023-03-04 19:18:12 +0200

    Add product class

--- backend/classes/Productlist.class.php
+++ backend/classes/Productlist.class.php
@@ -1,27 +1,27 @@
 <?php
 
 abstract class Product {
-    // protected $sku;
-    // protected $name;
-    // protected $price;
+    protected $sku;
+    protected $name;
+    protected $price;
     
-    // public function __construct($sku, $name, $price) {
-    //     $this->sku = $sku;
-    //     $this->name = $name;
-    //     $this->price = $price;
-    // }
+    public function __construct($sku, $name, $price) {
+        $this->sku = $sku;
+        $this->name = $name;
+        $this->price = $price;
+    }
 
-    // public function getSku() {
-    //     return $this->sku;
-    // }
+    public function getSku() {
+        return $this->sku;
+    }
 
-    // public function getName() {
-    //     return $this->name;
-    // }
+    public function getName() {
+        return $this->name;
+    }
 
-    // public function getPrice() {
-    //     return $this->price;
-    // }
+    public function getPrice() {
+        return $this->price;
+    }
 
     abstract public function getDetails();
 }
